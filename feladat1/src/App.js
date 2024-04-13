8953009c4eef2e13d72b23f9b87952f85f368e47
infotanar <szoftverfejleszto2016@gmail.com>
2024-04-13 13:21:07 +0200

    1. feladat

---
+++ feladat1/src/App.js
@@ -0,0 +1,73 @@
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+  const [hiba, setHiba] = useState("")
+  const [nev, setNev] = useState("")
+  const [pont, setPont] = useState('')
+  const [eredmenyek, setEredmenyek] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (nev.length < 3) {
+      setHiba("A név nem lehet 3 karakternél rövidebb!")
+      return
+    }
+    if (pont === '' || pont < 0 || pont > 100) {
+      setHiba("A pont 1 és 100 közötti szám lehet!")
+      return
+    }
+    setEredmenyek([...eredmenyek, { nev, pont }])
+    setNev('')
+    setPont('')
+    setHiba('')
+  }
+
+  return (
+    <div className="container my-5">
+      <h2 className='mb-3'>Vizsga</h2>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="nev" className='form-label'>
+            Név
+          </label>
+          <input
+            type="text"
+            className='form-control'
+            id='nev'
+            value={nev}
+            onChange={e => setNev(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="pont" className='form-label'>
+            Pont
+          </label>
+          <input
+            type="number"
+            className='form-control'
+            id='pont'
+            value={pont}
+            onChange={e => setPont(e.target.value)}
+          />
+        </div>
+        {hiba && <p className='alert alert-danger'>{hiba}</p>}
+        <button
+          type='submit'
+          className='btn btn-primary mb-3'
+          onClick={handleSubmit}>
+          Hozzáadás
+        </button>
+      </form>
+      <ul>
+        {eredmenyek.map(eredmeny => (
+          <li key={nev + pont} className={eredmeny.pont <= 50 ? 'text-danger' : ''}>
+            {eredmeny.nev}: {eredmeny.pont}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App;
---
+++ feladat1/src/App.test.js
@@ -0,0 +1,78 @@
+import { render, screen, wait } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+
+function typeintoForm({ nev, pont }) {
+  const nameElement = screen.getByRole('textbox', {
+    name: "Név"
+  })
+  const scoreElement = screen.getByLabelText("Pont");
+  if (nev) {
+    userEvent.type(nameElement, nev)
+  }
+  if (pont) {
+    userEvent.type(scoreElement, pont)
+  }
+  return { nameElement, scoreElement }
+}
+
+function clickOnSubmitButton() {
+  userEvent.click(screen.getByRole('button', { name: "Hozzáadás" }))
+}
+
+beforeEach(() => {
+  render(<App />);
+})
+describe('Képernyő elemek', () => {
+  test('Vannak üres input mezők', () => {
+    expect(screen.getByLabelText("Név").value).toBe("");
+    expect(screen.getByLabelText("Pont").value).toBe("");
+  });
+  test('Be lehet írni a nevet', () => {
+    const { nameElement } = typeintoForm({ nev: "Kis Béla" })
+    expect(nameElement.value).toBe("Kis Béla")
+  })
+  test('Be lehet írni a pontot és szám típusú a mező', () => {
+    const { scoreElement } = typeintoForm({ pont: "51" })
+    expect(scoreElement.value).toBe("51")
+    expect(scoreElement.type).toBe("number")
+  })
+  test('Van Hozzáadás gomb', () => {
+    const submitButton = screen.getByRole('button', { name: "Hozzáadás" })
+    expect(submitButton).toBeInTheDocument()
+  })
+})
+describe('Hibakezelés', () => {
+  test('Hibaüzenet 3 karakternél rövidebb név esetén', async () => {
+    typeintoForm({ nev: "aa" })
+    clickOnSubmitButton();
+    expect(await screen.findByText("A név nem lehet 3 karakternél rövidebb!")).toBeInTheDocument()
+  })
+  test('Hibaüzenet hibás pontszám esetén', async () => {
+    typeintoForm({ nev: "aaa", pont: "" })
+    clickOnSubmitButton();
+    expect(await screen.findByText("A pont 1 és 100 közötti szám lehet!")).toBeInTheDocument()
+    typeintoForm({ nev: "aaa", pont: "-1" })
+    clickOnSubmitButton();
+    expect(await screen.findByText("A pont 1 és 100 közötti szám lehet!")).toBeInTheDocument()
+    typeintoForm({ nev: "aaa", pont: "101" })
+    clickOnSubmitButton();
+    expect(await screen.findByText("A pont 1 és 100 közötti szám lehet!")).toBeInTheDocument()
+  })
+})
+describe('Lista', () => {
+  test('A sikeres vizsga adatai helyesen megjelennek', async () => {
+    typeintoForm({ nev: "Nagy Géza", pont: "78" })
+    clickOnSubmitButton();
+    const listElement = await screen.findByRole("listitem")
+    expect(listElement.textContent).toBe("Nagy Géza: 78")
+    expect(listElement.classList).not.toContain('text-danger')
+  })
+  test('A sikertelen vizsga adatai helyesen megjelennek', async () => {
+    typeintoForm({ nev: "Kis Ibolya", pont: "49" })
+    clickOnSubmitButton();
+    const listElement = await screen.findByRole("listitem")
+    expect(listElement.textContent).toBe("Kis Ibolya: 49")
+    expect(listElement.classList).toContain('text-danger')
+  })
+})
